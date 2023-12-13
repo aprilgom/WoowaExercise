ea7416e06c0b38167a9b7c356ab90c313ae87073
aprilgom <aprilgom@gmail.com>
2023-12-13 21:00:27 +0900

    feat: 숫자 제안 받기

--- kotlin-baseball-6/src/main/kotlin/baseball/Application.kt
+++ kotlin-baseball-6/src/main/kotlin/baseball/Application.kt
@@ -1,6 +1,9 @@
 package baseball
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
+const val NUMBER_LENGTH = 3
+const val NUMBER_LENGTH_EXCEED_ERR = "number_length_exceed"
 fun main() {
     TODO("프로그램 구현")
 }
@@ -14,4 +17,17 @@ fun createRandomNumber(): List<Int> {
         }
     }
     return computer
+}
+
+fun getSuggestionNumber(): List<Int> {
+    val input = Console.readLine()
+    require(input.length > NUMBER_LENGTH) {
+        NUMBER_LENGTH_EXCEED_ERR
+    }
+    val number = input.map {
+        val num = it.digitToInt()
+        require(num in 1..9)
+        num
+    }
+    return number
 }
