f077a2ccab4e5a64332e3229705a12fcf018c7b3
aprilgom <aprilgom@gmail.com>
2023-12-14 22:13:25 +0900

    feat: 메뉴를 추천해주는 기능 구현

--- kotlin-menu/src/main/kotlin/menu/LunchManager.kt
+++ kotlin-menu/src/main/kotlin/menu/LunchManager.kt
@@ -1,8 +1,9 @@
 package menu
 
 import menu.FoodCategory.*
-class LunchManager(private val guests: List<Guest>) {
+class LunchManager(guests: List<Guest>) {
     val categories = setCategories()
+    val menus = setMenus(guests)
     init {
         require(guests.size in 2..5)
     }
@@ -18,5 +19,35 @@ class LunchManager(private val guests: List<Guest>) {
             categories.add(newCategory)
         }
         return categories
+    }
+    private fun setMenus(guests: List<Guest>): Map<Guest, List<String>> {
+        val guestMenus = mutableMapOf<Guest, List<String>>()
+        for(guest in guests) {
+            val menus = recommendWeekMenus(guest)
+            guestMenus[guest] = menus
+        }
+        return guestMenus
+    }
+    private fun recommendWeekMenus(guest: Guest): List<String> {
+        val weekMenus = mutableListOf<String>()
+        var day = 0
+        while (true) {
+            val menu = recommendMenu(categories[day], guest)
+            val count = weekMenus.count { it == menu }
+            if (count > 3) {
+                continue
+            }
+            day++
+        }
+        return weekMenus
+    }
+    private fun recommendMenu(category: FoodCategory, guest: Guest): String {
+        while (true) {
+            val randomMenu = Menus.pickRandomMenuInCategory(category)
+            if (guest.isNotEdibleMenu(randomMenu)) {
+                continue
+            }
+            return randomMenu
+        }
     }
 }
--- kotlin-menu/src/main/kotlin/menu/WeekDay.kt
+++ kotlin-menu/src/main/kotlin/menu/WeekDay.kt
@@ -1,9 +1,9 @@
 package menu
 
-enum class Week(val index: Int) {
-    MONDAY(1),
-    TUESDAY(2),
-    WEDNESDAY(3),
-    THURSDAY(4),
-    FRIDAY(5),
+enum class Week {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
 }
